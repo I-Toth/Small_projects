12671071fa5b13d2a0cf52843e4a47c4a1979381
Istvan Toth <t.pepeistvan@gmail.com>
2022-06-20 08:00:52 +0200

    I've modify the code.

--- leap_year.py
+++ leap_year.py
@@ -1,12 +1,25 @@
-year = int(input("Which year do you want to check?."))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year.")
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
         else:
-            print("Not leap year.")
+            return True
     else:
-        print("Leap yea.r")
-else:
-    print("Not leap year.")
+        return False
+
+def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month"
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
